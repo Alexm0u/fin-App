3357c3898d3bfb97cdf51aecf233fb7a7049a9f0
Álex <alejandro.mcamps@bosonit.com>
2023-10-05 14:37:55 +0200

    Cambiando PrimeNG por Bootstrap + comienzo de enrutado

--- fin-app/src/app/app-routing.module.ts
+++ fin-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActualComponent } from './components/actual/actual.component';
+import { IngresosComponent } from './components/ingresos/ingresos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component: ActualComponent},
+  {path: 'ingresos', component: IngresosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
